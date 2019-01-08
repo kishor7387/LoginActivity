cd4f17d3550fc1cbbdcc457a029b03bbd035c9a1
Administrator <kulkarni.kk36@gmail.com>
2019-01-08 01:29:42 -0500

    update code

--- app/src/main/java/com/bankverify/loginpage/MainActivity.java
+++ app/src/main/java/com/bankverify/loginpage/MainActivity.java
@@ -9,5 +9,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
+
+    private void Test() {
+        int i = 1 + 1;
+
+
+    }
+
 }
